a83138a6e9f9314ee7514b30f088f8bcc4128e80
Paddy Wardle <paddy.wardle@yahoo.com>
2022-12-05 14:54:22 +0000

    day5 initial commit

---
+++ day5/part1.py
@@ -0,0 +1,35 @@
+if __name__ == "__main__":
+    stack_1 = ['F', 'R', 'W']
+    stack_2 = ['P', 'W', 'V', 'D', 'C', 'M', 'H', 'T']
+    stack_3 = ['L', 'N', 'Z', 'M', 'P']
+    stack_4 = ['R', 'H', 'C', 'J']
+    stack_5 = ['B', 'T', 'Q', 'H', 'G', 'P', 'C']
+    stack_6 = ['Z', 'F', 'L', 'W', 'C', 'G']
+    stack_7 = ['C', 'G', 'J', 'Z', 'Q', 'L', 'V', 'W']
+    stack_8 = ['C', 'V', 'T', 'W', 'F', 'R', 'N', 'P']
+    stack_9 = ['V', 'S', 'R', 'G', 'H', 'W', 'J']
+
+    stack_list = [stack_1, stack_2, stack_3, stack_4, stack_5, stack_6,
+                  stack_7, stack_8, stack_9]
+
+    with open('input.txt') as f:
+        for line in f.readlines():
+            
+            string_list = line.split()
+            num_crates = int(string_list[1])
+            start_stack_index = int(string_list[3]) - 1
+            finish_stack_index = int(string_list[5]) - 1
+            starting_stack = stack_list[start_stack_index]
+
+            for i in range(num_crates):
+                stack_list[finish_stack_index].insert(0, starting_stack[0])
+                stack_list[start_stack_index].pop(0)
+
+    final_answer = ''
+
+    for stack in stack_list:
+        final_answer += stack[0]
+
+    print(final_answer)
+
+
